95920ebb0a18c695f5f38ceec7cfbaf4aa89c07e
Zichen Wen <Zichen.Wen@outlook.com>
2025-03-30 23:16:38 +0800

    add FastVEuclidean by yifeng

--- LLaVA_FastV/llava/model/language_model/modeling_llama_self.py
+++ LLaVA_FastV/llava/model/language_model/modeling_llama_self.py
@@ -397,6 +397,185 @@ class FastV(LlamaModel):
             hidden_states=all_hidden_states,
             attentions=all_self_attns,
         )
+    
+
+class FastVEuclidean(LlamaModel):
+    def __init__(self, config: LlamaConfig):
+        self.last_attention = None
+        self.last_euclidean_distance = None
+        super().__init__(config)
+
+    def forward(
+        self,
+        input_ids: torch.LongTensor = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_values: Optional[List[torch.FloatTensor]] = None,
+        inputs_embeds: Optional[torch.FloatTensor] = None,
+        use_cache: Optional[bool] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+    ) -> Union[Tuple, BaseModelOutputWithPast]:
+
+        output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
+        output_hidden_states = (
+            output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
+        )
+        use_cache = use_cache if use_cache is not None else self.config.use_cache
+
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        # retrieve input_ids and inputs_embeds
+        if input_ids is not None and inputs_embeds is not None:
+            raise ValueError("You cannot specify both input_ids and inputs_embeds at the same time")
+        elif input_ids is not None:
+            batch_size, seq_length = input_ids.shape[:2]
+        elif inputs_embeds is not None:
+            batch_size, seq_length = inputs_embeds.shape[:2]
+        else:
+            raise ValueError("You have to specify either input_ids or inputs_embeds")
+
+        if self.gradient_checkpointing and self.training:
+            if use_cache:
+                print(
+                    "`use_cache=True` is incompatible with gradient checkpointing. Setting `use_cache=False`..."
+                )
+                use_cache = False
+
+        past_key_values_length = 0
+        if use_cache:
+            use_legacy_cache = not isinstance(past_key_values, Cache)
+            if use_legacy_cache:
+                past_key_values = DynamicCache.from_legacy_cache(past_key_values)
+            past_key_values_length = past_key_values.get_usable_length(seq_length)
+
+
+        if position_ids is None:
+            device = input_ids.device if input_ids is not None else inputs_embeds.device
+            position_ids = torch.arange(
+                past_key_values_length, seq_length + past_key_values_length, dtype=torch.long, device=device
+            )
+            position_ids = position_ids.unsqueeze(0)
+
+        if inputs_embeds is None:
+            inputs_embeds = self.embed_tokens(input_ids)
+
+        if self._use_flash_attention_2:
+            # 2d mask is passed through the layers
+            attention_mask = attention_mask if (attention_mask is not None and 0 in attention_mask) else None
+        elif self._use_sdpa and not output_attentions:
+            # output_attentions=True can not be supported when using SDPA, and we fall back on
+            # the manual implementation that requires a 4D causal mask in all cases.
+            attention_mask = _prepare_4d_causal_attention_mask_for_sdpa(
+                attention_mask,
+                (batch_size, seq_length),
+                inputs_embeds,
+                past_key_values_length,
+            )
+        else:
+            # 4d mask is passed through the layers
+            attention_mask = _prepare_4d_causal_attention_mask(
+                attention_mask, (batch_size, seq_length), inputs_embeds, past_key_values_length
+            )
+
+        # embed positions
+        hidden_states = inputs_embeds
+
+        # decoder layers
+        all_hidden_states = () if output_hidden_states else None
+        all_self_attns = () if output_attentions else None
+        next_decoder_cache = None
+
+        # print(f"using fastv!!!")
+        for decoder_layer in self.layers:
+            if output_hidden_states:
+                all_hidden_states += (hidden_states,)
+
+            if self.gradient_checkpointing and self.training:
+                layer_outputs = self._gradient_checkpointing_func(
+                    decoder_layer.__call__,
+                    hidden_states,
+                    attention_mask,
+                    position_ids,
+                    past_key_values,
+                    output_attentions,
+                    use_cache,
+                )
+            else:
+                sparse_config = self.config.sparse_config
+                if sparse_config is not None:
+                    K = sparse_config['K']  # pruned layer
+                    ratio = sparse_config['reduction_ratio']
+                    image_start = sparse_config['image_token_start_index']
+                    image_token_length = sparse_config['image_token_length']
+                    image_end = image_start + image_token_length
+                else:
+                    K = -1
+
+                if decoder_layer.self_attn.layer_idx == K and seq_length > 1:
+                    device = hidden_states.device
+
+                    euclidean_score = self.last_euclidean_distance[0][image_start:image_end]  # [image_token_length]
+
+                    image_attention_score = self.last_attention.mean(dim=1)[0][-1][image_start:image_end] 
+
+                    image_attention_score = euclidean_score * image_attention_score  # [image_token_length]
+
+                    top_attention_rank_index = image_attention_score.topk(int(image_token_length * (1 - ratio))).indices + image_start
+
+                    keep_indexs = torch.cat((torch.arange(image_start,device=device), top_attention_rank_index, torch.arange(image_end,seq_length,device=device)))
+                    keep_indexs = keep_indexs.sort().values
+                    # save keep_indexs
+
+                    hidden_states = hidden_states[:,keep_indexs,:]
+                    if attention_mask is not None:
+                        attention_mask = attention_mask[:,:,:hidden_states.shape[1],:hidden_states.shape[1]]
+                    position_ids = keep_indexs.unsqueeze(0)
+
+                if decoder_layer.self_attn.layer_idx == K - 1 and seq_length > 1:
+                    output_attentions = True
+                else:
+                    output_attentions = False
+
+                layer_outputs = decoder_layer(
+                    hidden_states,
+                    attention_mask=attention_mask,
+                    position_ids=position_ids,
+                    past_key_value=past_key_values,
+                    output_attentions=output_attentions,
+                    use_cache=use_cache,
+                )
+                if decoder_layer.self_attn.layer_idx == K - 1 and seq_length > 1:
+                    self.last_attention = layer_outputs[1]
+                    self.last_euclidean_distance = torch.sqrt(torch.sum((hidden_states - layer_outputs[0]) ** 2, dim=-1))  # [bs, seq_len]
+                
+            hidden_states = layer_outputs[0]
+
+            output_attentions = False
+            if use_cache:
+                next_decoder_cache = layer_outputs[2 if output_attentions else 1]
+
+            if output_attentions:
+                all_self_attns += (layer_outputs[1],)
+
+        hidden_states = self.norm(hidden_states)
+
+        # add hidden states from the last decoder layer
+        if output_hidden_states:
+            all_hidden_states += (hidden_states,)
+
+        next_cache = None
+        if use_cache:
+            next_cache = next_decoder_cache.to_legacy_cache() if use_legacy_cache else next_decoder_cache
+        if not return_dict:
+            return tuple(v for v in [hidden_states, next_cache, all_hidden_states, all_self_attns] if v is not None)
+        return BaseModelOutputWithPast(
+            last_hidden_state=hidden_states,
+            past_key_values=next_cache,
+            hidden_states=all_hidden_states,
+            attentions=all_self_attns,
+        )
 
 
 
